f7d8095a88bdefaaef65b69db5375550babeac07
Alvin Ababil <neoryumasil@gmail.com>
2026-01-31 10:29:35 +0800

    ADD : Merge MainMenu and Settings Scene

--- ArkavidiaGJ/Assets/Scripts/UtilityScripts/MainMenuManager.cs
+++ ArkavidiaGJ/Assets/Scripts/UtilityScripts/MainMenuManager.cs
@@ -7,11 +7,17 @@ public class MainMenuManager : MonoBehaviour
 {
     // Scene Name
     [SerializeField] private string _sceneName;
+    [SerializeField] private GameObject _settingCanvas;
 
     // Functional Button
     public void goToScene()
     {
         SceneManager.LoadScene(_sceneName);
+    }
+
+    public void ActivateCanvas()
+    {
+        _settingCanvas.SetActive(true);
     }
 
     // Exit
